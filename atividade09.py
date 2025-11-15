013fa6958a27351fc6ccef93df1b9909268f7afa
Wanderson Souza Sá <wandersonsouzasafilho@gmail.com>
2025-11-14 22:08:53 -0300

    Minha segunda atividade

---
+++ atividade09.py
@@ -0,0 +1,24 @@
+"""
+Atividade 3.1
+Classificador de Idade
+
+Crie um programa que solicite a idade do usuário e classifique-o
+em uma das seguintes categorias:
+
+*Criança (0-12 anos),
+*Adolescente (13-17 anos),
+*Adulto (18-59 anos) ou
+*Idoso (60 anos ou mais)."""
+
+idade = input("Me fale a sua idade: ")
+
+idade = int(idade)
+
+if idade < 12:
+    print("Você é criança")
+elif ((idade > 12) & (idade < 17)):
+    print("Você é adolescente")
+elif ((idade > 17) & (idade < 59)):
+    print("Você é um adulto")
+else :
+    print("Você é idoso")
